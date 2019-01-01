0a21e5251bf369c0d84bc09395d4c9abd23e0248
pratimsingha <pratim.singha@gmail.com>
2019-01-02 01:58:43 +0530

    initial commit

---
+++ src/main/java/com/pratim/demo/ProjectManagementToolApplication.java
@@ -0,0 +1,14 @@
+package com.pratim.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementToolApplication.class, args);
+	}
+
+}
+
